83939b7716bdcccc441cfe935cb2f40682b88571
Sondos Ahmed <swsw45080@gmail.com>
2026-03-13 05:11:58 +0300

    implement POST endpoint with pydantic validation and error handling

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ def home():
 @app.get("/books")
 def get_all_books():
     return db_books # إرجاع البيانات بصيغة JSON تلقائياً
+
+# 3. رابط إضافة كتاب جديد (POST) 
+@app.post("/books")
+def create_book(book: Book):
+    # التحقق من أن الرقم التعريفي (ID) غير مكرر - Error Handling 
+    for existing_book in db_books:
+        if existing_book["id"] == book.id:
+            raise HTTPException(status_code=400, detail="هذا الرقم التعريفي (ID) موجود مسبقاً، يرجى استخدام رقم آخر.")
+    
+    # تحويل البيانات القادمة من المتدرب/المستخدم إلى قاموس (Dictionary)g 
+    new_book_data = book.dict()
+    
+    # إضافة الكتاب للقائمة (قاعدة البيانات الوهمية)
+    db_books.append(new_book_data)
+    
+    return {"message": "تم إضافة الكتاب بنجاح!", "book": new_book_data}
