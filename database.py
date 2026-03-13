c504b561ba42efea5fd652db8bf222fa24d2ba2b
Sondos Ahmed <swsw45080@gmail.com>
2026-03-13 05:01:55 +0300

    move mock database to a separate filev for better organiztion

---
+++ database.py
@@ -0,0 +1,4 @@
+# هنا سنضع قائمة الكتب بعيداً عن ملف الـ main
+db_books = [
+    {"id": 1, "title": "Clean Code", "author": "Robert Martin", "year": 2008}
+]
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import Book
+from database import db_books  # استيراد القائمة من ملفها الجديد
 
 app = FastAPI()
 
-# قاعدة بيانات وهمية (قائمة بسيطة) لتخزين الكتب مؤقتاً
-db_books = [
-    {"id": 1, "title": "Clean Code", "author": "Robert Martin", "year": 2008}
-]
-
+# 1. رابط الصفحة الرئيسية (GET)
 @app.get("/")
 def home():
     return {"message": "مرحباً بك في نظام إدارة الكتب!"}
 
+# 2. رابط عرض كل الكتب (GET) 
 @app.get("/books")
 def get_all_books():
     return db_books # إرجاع البيانات بصيغة JSON تلقائياً
-
